ef4f6a7625810384e1850455b22d711317b9cefd
emilakerman <emilaakerman@gmail.com>
2024-05-16 16:22:30 +0200

    added basic functonality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +13,95 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: InputTextField(),
         ),
       ),
+    );
+  }
+}
+
+class InputTextField extends StatefulWidget {
+  const InputTextField({
+    super.key,
+  });
+
+  @override
+  State<InputTextField> createState() => _InputTextFieldState();
+}
+
+class _InputTextFieldState extends State<InputTextField> {
+  final TextEditingController _controller = TextEditingController();
+  dynamic jsonData = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  /// example https://api.tvmaze.com/shows
+  ///
+  Future<dynamic> analyzeEndpoint(String endpoint) async {
+    String url = endpoint;
+    final Response response = await http.get(Uri.parse(url));
+    if (response.statusCode != 200) {
+      print('Error');
+      return [];
+    } else {
+      print('Success');
+      setState(() {
+        jsonData = response.body;
+      });
+      return response.body;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 300,
+              child: CupertinoTextField(
+                controller: _controller,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => analyzeEndpoint(_controller.text),
+              child: const Text(
+                'Analyze Endpoint',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => setState(() {
+                jsonData = [];
+              }),
+              child: const Text(
+                'Undo',
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: TextField(
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            controller: TextEditingController(text: jsonData.toString()),
+          ),
+        )
+      ],
     );
   }
 }
